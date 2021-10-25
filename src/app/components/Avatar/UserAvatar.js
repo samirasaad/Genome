971870bf1ddd2cd152a52607377a8cb8292c3f29
Samira <samirasaad@gmail.com>
2021-10-25 11:26:06 +0200

    adding placeholder for user avatr

--- src/app/components/Avatar/UserAvatar.js
+++ src/app/components/Avatar/UserAvatar.js
@@ -3,11 +3,12 @@ import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 
 const UserAvatar = ({ width, height, src }) => {
+  console.log(src);
   return (
     <Stack direction="row" spacing={3}>
       <Avatar
-        alt="Remy Sharp"
-        src="/static/images/avatar/1.jpg"
+        // alt="Remy Sharp" //if there is name the avatr will be the first leetre of it taken from alt
+        src={src ? src : "/broken-image.jpg"}
         sx={{ width: width, height: height }}
       />
     </Stack>
